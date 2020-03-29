06403fce49fb5b9e00847b9c91cfce24b1e4b1e7
SeraphyBR <luisjuniorbr@gmail.com>
2020-03-29 19:41:48 -0300

    Add new incident page

---
+++ frontend/src/pages/Profile/index.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { FiPower, FiTrash2 } from "react-icons/fi";
+
+import "./styles.css";
+
+import logoImg from "../../assets/logo.svg";
+
+function Profile() {
+    return (
+        <div className="profile-container">
+            <header>
+                <img src={logoImg} alt="Be The Hero" />
+                <span>Bem vinda, APAP</span>
+
+                <Link className="button" to="/incidents/new">
+                    Cadastrar novo caso
+                </Link>
+                <button type="button">
+                    <FiPower size={18} color="#e02041" />
+                </button>
+            </header>
+
+            <h1>Casos cadastrados</h1>
+
+            <ul>
+                <li>
+                    <strong>CASO:</strong>
+                    <p>Caso Teste</p>
+
+                    <strong>DESCRIÇÃO:</strong>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Placeat, corrupti dolorem quo porro, ut atque vitae
+                        possimus voluptates distinctio ab ad eum quod! Sit
+                        voluptas eligendi iusto obcaecati, fugit animi.
+                    </p>
+
+                    <strong>VALOR:</strong>
+                    <p>R$ 120,00</p>
+
+                    <button type="button">
+                        <FiTrash2 size={20} color="#a8a8b3"/>
+                    </button>
+                </li>
+            </ul>
+        </div>
+    );
+}
+
+export default Profile;
