5f6a89d9a129fc48e3f154c9a60a9fee01444399
SeraphyBR <luisjuniorbr@gmail.com>
2020-06-01 09:08:52 -0300

    Add mobile

---
+++ mobile/src/pages/Detail/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native';
+
+function Detail(): JSX.Element {
+    return(
+        <View>
+
+        </View>
+    );
+}
+
+export default Detail;
