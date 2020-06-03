3ce9b2e7d002a2d4bc22d9e926f3fa7ea7456ab1
SeraphyBR <luisjuniorbr@gmail.com>
2020-06-03 10:24:27 -0300

    Using ts-node

--- backend/src/database/connection.ts
+++ backend/src/database/connection.ts
@@ -1,6 +1,15 @@
 import knex from "knex";
-import config from "./knexfile";
+import path from "path";
 
-const connection = knex(config.development);
+const connection = knex({
+    client: 'sqlite3',
+    connection: {
+        filename: path.resolve(__dirname, 'db.sqlite')
+    },
+    migrations: {
+        directory: path.resolve(__dirname, 'migrations')
+    },
+    useNullAsDefault: true
+});
 
 export default connection;
